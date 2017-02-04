0f45989c644ba6da61d600104ecaa90bec8392c7
W. Brian Gourlie <bgourlie@gmail.com>
2017-02-04 15:07:10 -0600

    Port disassembler from rs-nes

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,9 @@
 extern crate nom;
 
 mod assembler;
+mod disassembler;
 mod parser;
 mod tokens;
 
 pub use assembler::assemble;
+pub use disassembler::{AddressingMode, Instruction, InstructionDecoder, Mnemonic};
